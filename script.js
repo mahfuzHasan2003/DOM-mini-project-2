4b7d470ce26d1cdb6278abf41653d9f82409c2f5
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2024-09-23 15:47:24 +0600

    added history data

--- script.js
+++ script.js
@@ -11,35 +11,104 @@ function getInputValue(ID) {
   return parseInt(document.getElementById(ID).value);
 }
 
-// check input validity
-function checkInputIsValid(ID) {
-  if (getInputValue(ID) <= 0 || isNaN(getInputValue(ID) <= 0)) {
+// ---------------------check donation input validity-----------------------------
+function checkInputIsValid(val, tbalance) {
+  if (val <= 0 || isNaN(val) || tbalance < val) {
     return false;
   }
   return true;
 }
 
-//  --------------------------------------------------------------------------
-// Home to Blog
+//  ------------------------Home to Blog page-------------------------------
 getElement("blog-btn").addEventListener("click", function () {
   window.location.href = "./blog.html";
 });
 
-// tab switching from Donation to History & History to Donation
+// --------tab switching from Donation to History & History to Donation------
 const btns = document.querySelectorAll(".tab-btns button");
 for (const EveryBtn of btns) {
   EveryBtn.addEventListener("click", function (e) {
     for (const i of btns) {
       i.classList.remove("tab-btn-active");
       getElement(i.getAttribute("data-details")).classList.add("hidden");
+      document.querySelector("footer").classList.remove("hidden");
     }
     e.target.classList.add("tab-btn-active");
     getElement(e.target.getAttribute("data-details")).classList.remove(
       "hidden"
     );
+    if (e.target.innerText === "History") {
+      document.querySelector("footer").classList.add("hidden");
+    }
   });
 }
 
-// Assigning variable to frequently use
-const totalBalance = getElementInnerText("total-balance");
-console.log(totalBalance);
+// ---------------add my donation data to the history page----------------------
+function addHistory(donatedTaka, forWhat) {
+  forWhat = forWhat.split(" ").slice(2).join(" ");
+  getElement(
+    "history-details"
+  ).innerHTML = `<div class="space-y-1 border-2 border-[#ffffff33] p-5 rounded-md">
+          <h4 class="text-lg font-bold">
+            ${donatedTaka} Taka is Donated for ${forWhat}
+          </h4>
+          <p>
+            Date : ${new Date()}
+          </p>
+        </div> ${getElement("history-details").innerHTML}`;
+}
+
+//  -----------------------Donate Now btn functionality------------------------------
+const donateNowBtn = document.querySelectorAll(".donate-now-btn");
+
+for (const btn of donateNowBtn) {
+  btn.addEventListener("click", function (e) {
+    const totalBalance = getElementInnerText("total-balance");
+    let soFarDonatedElement =
+      e.target.parentElement.querySelector(".donated-amount");
+    let soFarDonatedAmount = parseInt(
+      e.target.parentElement.querySelector(".donated-amount").innerText
+    );
+    const myDonationElement =
+      e.target.parentElement.querySelector(".donation-amount");
+    const myDonationAmount = parseInt(
+      e.target.parentElement.querySelector(".donation-amount").value
+    );
+
+    // handling Error
+    if (!checkInputIsValid(myDonationAmount, totalBalance)) {
+      e.target.parentElement
+        .querySelector(".invalid-warning")
+        .classList.remove("hidden");
+      return;
+    }
+
+    if (checkInputIsValid(myDonationAmount, totalBalance)) {
+      e.target.parentElement
+        .querySelector(".invalid-warning")
+        .classList.add("hidden");
+      // -----------updating so Far Donattion---------
+      soFarDonatedElement.innerText = myDonationAmount + soFarDonatedAmount;
+      // ----------updating total balance--------
+      getElement("total-balance").innerText = totalBalance - myDonationAmount;
+      // ----------showing modal------------
+      getElement("modal").style.display = "grid";
+      // ----------remove modal-------------
+      getElement("modal").addEventListener("click", function (e) {
+        if (e.target.innerText === "Close Confirmation") {
+          getElement("modal").style.display = "none";
+        }
+      });
+      // -------------------clearing input value--------------
+      myDonationElement.value = "";
+
+      // -------------------passig data for history--------------
+      addHistory(
+        myDonationAmount,
+        e.target.parentElement.querySelector(".donation-title").innerText
+      );
+    }
+  });
+}
+//  ----------------------------Donate Now Ends------------------------------------
+// console.log(totalBalance);
